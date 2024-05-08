a68b41c6bf5913ab79e8ab847fb46145f1a2ca4d
Victor Hugo Jimenez T <victor.jimenezt@perficient.com>
2024-05-08 16:53:20 -0500

    vars

--- collector-lib.go
+++ collector-lib.go
@@ -1,10 +1,15 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"strconv"
 )
 
 func main() {
-    message := "Victor"
-    fmt.Println(message)
+	const percentageOfRevenue = 0.15
+	message := "Victor you have : "
+	numberOfRecords := 1200
+	var stimatedTotalValue float64 = 60000 + (60000 * percentageOfRevenue)
+
+	fmt.Println(message + strconv.Itoa(numberOfRecords) + " records with a stimated value of " + strconv.FormatFloat(stimatedTotalValue, 'f', -1, 64))
 }
